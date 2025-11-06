b0e146add5bcb66400b6bbe3340483c0c559fb1b
Giorgia Pivi <giorgia.pivi@studio.unibo.it>
2025-11-06 14:56:50 +0100

    risoluzione dei TODO

--- 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 73-junit/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -5,6 +5,8 @@ import it.unibo.bank.api.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
@@ -21,7 +23,9 @@ class TestStrictBankAccount {
      */
     @BeforeEach
     public void setUp() {
-        fail("To be implemented");
+        this.mRossi = new AccountHolder("mario","rossi", 1);
+        this.bankAccount = new StrictBankAccount(mRossi, 0.0);
+        //fail("To be implemented");
     }
 
     /**
@@ -29,7 +33,10 @@ class TestStrictBankAccount {
      */
     @Test
     public void testInitialization() {
-        fail("To be implemented");
+        assertEquals(0.0, bankAccount.getBalance());
+        assertEquals(0, bankAccount.getTransactionsCount());
+        assertEquals(mRossi, bankAccount.getAccountHolder());
+        //fail("To be implemented");
     }
 
     /**
@@ -37,7 +44,10 @@ class TestStrictBankAccount {
      */
     @Test
     public void testManagementFees() {
-        fail("To be implemented");
+        bankAccount.deposit(1, 100);
+        bankAccount.chargeManagementFees(1);
+        assertEquals(93, bankAccount.getBalance());
+       //fail("To be implemented");
     }
 
     /**
@@ -45,7 +55,8 @@ class TestStrictBankAccount {
      */
     @Test
     public void testNegativeWithdraw() {
-        fail("To be implemented");
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(1, -100));
+       // fail("To be implemented");
     }
 
     /**
@@ -53,6 +64,8 @@ class TestStrictBankAccount {
      */
     @Test
     public void testWithdrawingTooMuch() {
-        fail("To be implemented");
+        bankAccount.deposit(1, 50);
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(1, 100));
+        //fail("To be implemented");
     }
 }
